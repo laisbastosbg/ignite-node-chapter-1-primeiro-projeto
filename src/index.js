85114aca4f0fe41252645a8f4075e251991c1e88
Lais <lais@pop-os.localdomain>
2021-08-26 00:49:38 -0300

    deletar conta ok / obter saldo ok

--- src/index.js
+++ src/index.js
@@ -140,4 +140,19 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const customerIndex = customers.indexOf(customer);
+  customers.splice(customerIndex, 1);
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+  
+  return response.json(balance);
+})
+
 app.listen(3333, () => console.log("Server listening on port 3333"));
